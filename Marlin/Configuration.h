ebd83b5adc1c1c3aec545ca650c4c7ad99d8a952
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2018-07-22 11:58:07 +0200

    Set HOMING_FEEDRATE_*.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1041,8 +1041,8 @@
 #endif
 
 // Homing speeds (mm/m)
-#define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (4*60)
+#define HOMING_FEEDRATE_XY (40*60)
+#define HOMING_FEEDRATE_Z  (3*60)
 
 // @section calibrate
 
